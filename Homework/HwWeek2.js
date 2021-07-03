99fd38eb929d0f4bda98561adbaa9e589b4b127d
Yonnie Hicks <yhicks25@gmail.com>
2021-07-03 10:50:35 -0500

    Update HwWeek2.js

---
+++ Homework/HwWeek2.js
@@ -0,0 +1,7 @@
+const pizzaPlace = "Basil Pizza";
+let numberOfToppings = 30;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+
+console.log(`${pizzaPlace} has ${numberOfToppings} great toppings!`);
