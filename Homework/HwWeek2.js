0dfd83ab526a70fcd755265dfea9f8b109f50c26
Yonnie Hicks <yhicks25@gmail.com>
2021-07-06 13:30:41 -0500

    Delete HwWeek2.js

--- Homework/HwWeek2.js
+++
@@ -1,7 +0,0 @@
-const pizzaPlace = "Basil Pizza";
-let numberOfToppings = 30;
-
-console.log(pizzaPlace);
-console.log(numberOfToppings);
-
-console.log(`${pizzaPlace} has ${numberOfToppings} great toppings!`);
