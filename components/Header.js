e3b2fc755d44be8a24f266fd331d6ee6185cc83e
Yonnie Hicks <yhicks25@gmail.com>
2021-08-07 21:14:16 -0500

    update

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,6 @@ import html from "html-literal";
 export default st => html`
   <header>
     <h1>@ Home Wellness ${st.header}</h1>
-    <div class="logo-container">
-      <img src="images/WellnessLogo.jpg" alt="logo" />
-    </div>
-    <nav>
-      <ul class="nav-links">
-        <li><a class="nav-link" href="topics.html">Topics</a></li>
-        <li><a class="nav-link" href="about.html">About</a></li>
-        <li><a class="nav-link" href="contact.html">Contact</a></li>
-      </ul>
-    </nav>
+    <div class="logo-container"></div>
   </header>
 `;
