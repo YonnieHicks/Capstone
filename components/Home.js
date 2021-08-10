7393e6249ca9dcefa7c1f67d76f5546c1f8596a4
Yonnie Hicks <yhicks25@gmail.com>
2021-08-10 17:43:49 -0500

    update

--- components/Home.js
+++
@@ -1,5 +0,0 @@
-const kelvinToFahrenheit = kelvinTemp =>
-  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
-
-Weather in $;{st.weather.city} $;{kelvinToFahrenheit(st.weather.temp)}F, feels
-    like${kelvinToFahrenheit(st.weather.feelsLike)}F
