84f566d8314646b2a4e697ed9f5e9f3e3798a8d0
Yonnie Hicks <yhicks25@gmail.com>
2021-08-16 17:27:35 -0500

    update

--- components/views/Presentation.js
+++ components/views/Presentation.js
@@ -9,5 +9,10 @@ export default () => `
   </div>
 </class>
 </section>
+const kelvinToFahrenheit = kelvinTemp =>
+  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+Weather in ${st.weather.city} ${kelvinToFahrenheit(st.weather.temp)}F, feels
+    like ${kelvinToFahrenheit(st.weather.feelsLike)}F
 
 `;
