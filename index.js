d03271848c119a26aa8d8cabeda596fd4d9a7a03
Yonnie Hicks <yhicks25@gmail.com>
2021-08-18 13:46:30 -0500

    week 8 homework update

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-/* eslint-disable no-prototype-builtins */
 import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
-
 import Navigo from "navigo";
 import { capitalize } from "lodash";
-import "./env";
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = new Navigo(window.location.origin);
-
-router
-  .on({
-    ":page": params => render(state[capitalize(params.page)]),
-    "/": () => render(state.Home)
-  })
-  .resolve();
+console.log('matsinet-process.env:', process.env);
 
 function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
@@ -31,8 +25,8 @@ function render(st = state.Home) {
 
 function addEventListeners(st) {
   // add event listeners to Nav items for navigation
-  document.querySelectorAll("nav a").forEach(navLink =>
-    navLink.addEventListener("click", event => {
+  document.querySelectorAll("nav a").forEach((navLink) =>
+    navLink.addEventListener("click", (event) => {
       event.preventDefault();
       render(state[event.target.title]);
     })
@@ -45,88 +39,38 @@ function addEventListeners(st) {
       document.querySelector("nav > ul").classList.toggle("hidden--mobile")
     );
 
-  // event listener for the the photo form
-  if (st.view === "Form") {
-    document.querySelector("form").addEventListener("submit", event => {
-      event.preventDefault();
-      // convert HTML elements to Array
-      let inputList = Array.from(event.target.elements);
-      // remove submit button from list
-      inputList.pop();
-      // construct new picture object
-      let newPic = inputList.reduce((pictureObject, input) => {
-        pictureObject[input.name] = input.value;
-        return pictureObject;
-      }, {});
-      // add new picture to state.Gallery.pictures
-      state.Gallery.pictures.push(newPic);
-      render(state.Gallery);
-    });
-  }
-}
-
-// get data from an API endpoint
-// axios
-//   .get("https://jsonplaceholder.typicode.com/posts")
-//   // handle the response from the API
-//   .then((response) => {
-//     // for each post in the response Array,
-//     response.data.forEach((post) => {
-//       // add it to state.Blog.posts
-//       state.Blog.posts.push(post);
-//     });
-//   });
-
 router.hooks({
   before: (done, params) => {
-    const page =
-      params && params.hasOwnProperty("page")
-        ? capitalize(params.page)
-        : "Home";
+    const page = params && params.hasOwnProperty("page") ? capitalize(params.page) : "Home";
 
     switch (page) {
-      case "Blog":
-        state.Blog.posts = [];
-        axios
-          .get("https://jsonplaceholder.typicode.com/posts/")
-          .then(response => {
-            response.data.forEach(post => {
-              state.Blog.posts.push(post);
-            });
-            done();
-            console.log(state.Blog.posts);
-          })
-          .catch(err => console.log(err));
-        break;
 
-      case "Home":
-        axios
-          .get(
-            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.WEATHER_API_KEY}&q=st.%20louis`
-          )
-          .then(response => {
+          case "Home":
+          axios
+            .get(
+              `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.WEATHER_API_KEY}&q=st.%20louis`
+            )
+          .then((response) => {
             state.Home.weather = {};
-            console.log(response, state.Home.weather);
             state.Home.weather.city = response.data.name;
             state.Home.weather.temp = response.data.main.temp;
             state.Home.weather.feelsLike = response.data.main.feels_like;
             state.Home.weather.humidity = response.data.main.humidity;
-            state.Home.weather.description =
-              response.data.weather[0]["description"];
+            state.Home.weather.description = response.data.weather[0]["description"];
             done();
           })
-          .catch(err => console.log(err));
+          .catch((err) => console.log(err));
         break;
 
       default:
         done();
     }
-  }
+  },
 });
 
 router
   .on({
     "/": () => render(state.Home),
-    ":page": params => render(state[capitalize(params.page)])
+    ":page": (params) => render(state[capitalize(params.page)]),
   })
-  .resolve();
+  .resolve()};
