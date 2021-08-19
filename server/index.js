00ee6779ade38a97103af076cac701eaf8917bb3
Yonnie Hicks <yhicks25@gmail.com>
2021-08-19 09:50:49 -0500

    update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-const greetings = require("./routers/greetings");
+//const greetings = require("./routers/greetings");
 
 
 dotenv.config();
--- server/routers/greetings.js
+++ server/routers/greetings.js
@@ -1,11 +1,11 @@
-const { Router } = require("express");
-const router = Router();
+// const { Router } = require("express");
+// const router = Router();
 
-router
-  .route("/greetings/:name")
-  .get((request, response) => {
-    const name = request.params.name;
-    response.status(418).json({ message: `Hello ${name}` });
-  });
+// router
+//   .route("/greetings/:name")
+//   .get((request, response) => {
+//     const name = request.params.name;
+//     response.status(418).json({ message: `Hello ${name}` });
+//   });
 
-module.exports = router;
+// module.exports = router;
