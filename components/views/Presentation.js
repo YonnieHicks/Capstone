1934be2e6d87b08c9b6a4fee27358e6c5954eb48
Yonnie Hicks <yhicks25@gmail.com>
2021-08-19 09:30:39 -0500

    express server setup

--- components/views/Presentation.js
+++ components/views/Presentation.js
@@ -1,16 +1,20 @@
-export default () => `
+import html from "html-literal";
+
+const kelvinToFahrenheit = kelvinTemp =>
+  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+
+export default (st) => html`
 <section class="presentation">
-<class class="introduction">
+<div class="introduction">
   <div class="intro-text">
     <h1>Ipsum lorem ips </h1>
     <p>
     Ipsum lorem ips
     </p>
   </div>
-</class>
+</div>
 </section>
-const kelvinToFahrenheit = kelvinTemp =>
-  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
 
 Weather in ${st.weather.city} ${kelvinToFahrenheit(st.weather.temp)}F, feels
     like ${kelvinToFahrenheit(st.weather.feelsLike)}F
