81db32dcc0bdd12371b17a6fba182316bdd8f041
Yonnie Hicks <yhicks25@gmail.com>
2021-09-02 10:03:43 -0500

    update

--- components/views/Astrology.js
+++ components/views/Astrology.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 
 export default (st) => html`
-  <section id="astrology">
+  <section id="Astrology">
     ${st.posts
       .map((post) => {
         return formatAstrologyPost(post);
@@ -9,3 +9,14 @@ export default (st) => html`
       .join("")}
   </section>
 `;
+function formatAstrologyPost(post) {
+  return `
+  <div class="astrology-post">
+    <h1>${post.sign}</h1>
+    <h1>by User ${post.day}</h1>
+
+    <hr>
+    <br/>
+  </div>`;
+}
+
--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
-const greetings = require("./routers/greetings");
+const axios = require("axios");
 
 
 dotenv.config();
