e2fdaea765c6a0a03bd2ed37b5078ac5d228bdcf
Yonnie Hicks <yhicks25@gmail.com>
2021-09-07 14:39:18 -0500

    working music page

--- components/views/Music.js
+++ components/views/Music.js
@@ -3,17 +3,14 @@ import music from "../../assets/images/videos/music.mp4";
 
 export default (st) => html`
 
-<img src="${music}" alt="me" />
 
-<video controls width="absolute">
+<video controls width="750">
 
-    <source src="${music}"
-            type="video/webm">
+        <source src="${music}" type="video/webm">
 
-    <source src="${music}"
-            type="video/mp4">
+        <source src="${music}" type="video/mp4">
 
-    Sorry, your browser doesn't support embedded videos.
+        Sorry, your browser doesn't support embedded videos.
 </video>
 
 
