15dc5517928adc84b7da401e6783580336b9e360
Yonnie Hicks <yhicks25@gmail.com>
2021-09-07 16:15:42 -0500

    working music and jokes page

--- components/views/Affirmations.js
+++ components/views/Affirmations.js
@@ -1,22 +1,9 @@
 import html from "html-literal";
 
 export default (st) => html`
-  <section id="Affirmations">
-    ${st.posts
-      .map((post) => {
-        return formatAffirmationsPost(post);
-      })
-      .join("")}
-  </section>
+
+
+
+
+
 `;
-function formatAffirmationsPost(post) {
-  return `
-  <div class="affirmations-post">
-    <h1>${post.sign}</h1>
-    <h1>by User ${post.day}</h1>
-
-    <hr>
-    <br/>
-  </div>`;
-}
-
