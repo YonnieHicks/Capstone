db5508813cc0a4e7bdeae82d8918a76c460039c2
Yonnie Hicks <yhicks25@gmail.com>
2021-09-07 14:20:19 -0500

    working music page

--- components/views/Music.js
+++ components/views/Music.js
@@ -1,3 +1,21 @@
 import html from "html-literal";
+import music from "../../assets/images/videos/music.mp4";
 
-export default (st) => html``
+export default (st) => html`
+
+<img src="${music}" alt="me" />
+
+<video controls width="absolute">
+
+    <source src="${music}"
+            type="video/webm">
+
+    <source src="${music}"
+            type="video/mp4">
+
+    Sorry, your browser doesn't support embedded videos.
+</video>
+
+
+`;
+
--- store/Music.js
+++ store/Music.js
@@ -1,5 +1,5 @@
 export default {
   header: "Music",
   view: "Music",
-  readings: []
+  music: []
 };
