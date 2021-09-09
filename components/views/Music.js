53d9ea8787b6c0fc40c8f0e14d3017f779563fae
Yonnie Hicks <yhicks25@gmail.com>
2021-09-09 14:17:26 -0500

    update

--- components/views/Music.js
+++ components/views/Music.js
@@ -1,15 +1,12 @@
+
 import html from "html-literal";
-import music from "../../assets/images/videos/music.mp4";
 
 export default (st) => html`
 
-<video class="center"  controls width="600";height="auto">
-
-<source src="${music}" type="video/mp4">
 
 
-</video>
+
+
 
 
 `;
-
