c67ab67153920114c7ffeb5315e6080f9289ad10
YonnieHicks <85805865+YonnieHicks@users.noreply.github.com>
2022-07-22 13:01:05 -0500

    jokes deleted

--- components/views/Jokes.js
+++ components/views/Jokes.js
@@ -15,11 +15,6 @@ function formatJokesjokes(jokes) {
  <center> <div class="jokes">
 
  <section id="jumbotron">
- <h2><underline>Joke of the Day</underline></h2>
- </section>
-
-   <center> <h3>A lion would never play golf,</h3><br>
-    <h3>but a Tiger Wood! </h3></center>
 
 
 
